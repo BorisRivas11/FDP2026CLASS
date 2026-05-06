5ccf3bd1f9c668c17afd27c51dfec0a918f0b5c4
BorisRivas11 <00082826@uca.edu.sv>
2026-05-06 11:19:19 -0600

    Fundamentos

---
+++ Ejercicio 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int numero1, numero2 ;
+
+    cout<<"ejercicios suma de 2 numero"endl;
+    cin >> a;
+
+    cout<<"Ingrese el primer numero">>;
+    in >> numero1;
+    << "Ingrese el segundo numero:";
+    in >> numero2;
+
+    out << "La suma de los dos numeros es:" <<*(numero1 + numero2) << endl;
+
+    return 0;
+    
+}
