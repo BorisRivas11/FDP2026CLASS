e4733ea34ac8534788d8b5d76490a8f1f25fdc38
BorisRivas11 <00082826@uca.edu.sv>
2026-05-18 12:37:26 -0600

    CLASE FOR 18/5

---
+++ FOR.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int n=0, a;
+    cout << "Ingrese un numero que sera tu limite: " << endl;
+    cin >> a;
+    for (int i= 1 ; i <= a; i++){
+        cout << i << " ";
+        cout << endl;
+
+    }
+    
+    return 0;
+
+}
---
+++ for2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;               
+    bool esPrimo = true; 
+
+    
+    cout << "Ingresa un numero entero: ";
+    cin >> n;
+
+
+    if (n <= 1)
+    {
+        esPrimo = false; 
+    }
+    else
+    {
+        
+        for (int i = 2; i * i <= n; i++)
+        {
+            cout<<n<<endl;
+            cout<<i<<endl;
+            if (n % i == 0)
+            {                    
+                esPrimo = false; 
+            }
+        }
+    }
+
+    
+    if (esPrimo)
+    {
+        cout << n << " es un numero primo." << endl; \
+    }
+    else
+    {
+        cout << n << " no es un numero primo." << endl; 
+    }
+
+    return 0; 
+}
